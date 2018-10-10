683ac9289e839b63fedc0b02a06b158d5723f069
Robert Elizalde <>
2018-10-10 16:34:47 -0500

    4 quadrants beta

--- src/automatedrawer/State.java
+++ src/automatedrawer/State.java
@@ -57,11 +57,23 @@ public class State {
         System.out.println("x = " + pointOfConnection[0] + " , Y = " + pointOfConnection[1]);
         return pointOfConnection;
     }
-    public int[] getPointOfConnection(int xR){
-        System.out.println(this.maxYRange);
-        System.out.println(this.minYRange);
+    public int[] getPointOfConnection(int xR, int yR){
         int pointOfConnection[] = new int[2];
-        int y = (int)(Math.random()*(this.maxYRange - this.minYRange + 1)) + this.minYRange;
+        int y = getY();
+        System.out.println("yR=" + yR + " y getY()=" + getY() );
+        //Above the state 
+        if (yR < getY()){
+            y = (int)(Math.random()*(getY() - this.minYRange + 1)) + this.minYRange;
+        }
+        //Down the state
+        if(yR > getY()){
+            y = (int)(Math.random()*(this.maxYRange - getY() + 1)) + getY();
+        }
+        //Y equals
+        if(yR==getY()){
+            y = yR;
+        }
+        // y = (int)(Math.random()*(this.maxYRange - this.minYRange + 1)) + this.minYRange;
         int x;
         if (xR < getX()) {
             x = -(int)Math.sqrt((d/2)*(d/2) - (y-this.y)*(y-this.y)) + (this.x);
@@ -71,7 +83,6 @@ public class State {
         }
         pointOfConnection[0] = x;
         pointOfConnection[1] = y;
-        System.out.println("x = " + pointOfConnection[0] + " , Y = " + pointOfConnection[1]);
         return pointOfConnection;
     }
 }
